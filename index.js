39f714aa0edc6c07a8dacae9671639e852f278a8
Fabrício Barretti <fabriciobarretti@gmail.com>
2024-07-17 08:26:35 -0300

    Still fixing issues.

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ table.innerHTML = ``;
 // testIdentification();
 genExercise(3);
 
-function testIdentification(){
-    let bases = identifyBase(2,"add",10); 
-    console.log(`Base 5: ${bases.isBase5} | Base 10: ${bases.isBase10}`);
-};
-
 // Isert base as a parameter here later in order to be possible for the user to customize the calculations
 function genExercise (size){ 
     let exercise = [];
@@ -24,34 +19,32 @@ function genExercise (size){
             exercise[i] = Math.floor(Math.random() * 9) + 1; //Forces the first to be positive and greater than 0
         } else {
             exercise[i] = (Math.floor(Math.random() * 9) + 1) * operation.multiplier;
+            console.log("Operation: " + operation.name);
         }
         console.log("Generated number:" + exercise[i]);
+        
+        let opBase = identifyBase(abacusNumber,operation,exercise[i]);  
+        // console.log(opBase);
 
-        let opBase = identifyBase(abacusNumber,operation,exercise[i]);
-        operation.isBase5 = opBase.isBase5;
-        operation.isBase10 = opBase.isBase10;
-
-        if (operation.multiplier == 1 && exercise[i] != 1){
+        if (i > 1){
             // console.log(`Operation: ${abacusNumber}+${exercise[i]}`);
-            console.log(`Operation mult: ${operation.multiplier}`);
+            // console.log(`Operation multiplier: ${operation.symbol}`);
+            console.log("Base 5: " + opBase.isBase5);
+            console.log("Base 10: " + opBase.isBase10);
             
-        } else if (operation.multiplier == -1 && exercise[i] != 1){
-            // console.log(`Operation: ${abacusNumber}-${exercise[i]}`);
-            console.log(`Operation mult: ${operation.multiplier}`);
         }
 
-        console.log("Base 5: " + operation.isBase5);
-        console.log("Base 10: " + operation.isBase10);
-
         
-        // while (!opBase.isBase5){
-        //     exercise[i] = Math.floor(Math.random() * 10) * operation;
-        //     let reviewedBase = identifyBase(abacusNumber,operation,exercise[i]);
-        //     console.log("Second number base: " + reviewedBase);
-        //     console.log("Second 10umber base: " + reviewedBas10);
 
-        //     if (reviewedBase.isBase5) break;
-        // };
+        // This would create a Base-5 exercise
+        // op while (!opBase.isBase5){
+        // op     exercise[i] = Math.floor(Math.random() * 10) * operation;
+        // //     let reviewedBase = identifyBase(abacusNumber,operation,exercise[i]);
+        // //     console.log("Second number base: " + reviewedBase);
+        // //     console.log("Second 10umber base: " + reviewedBas10);
+
+        // //     if (reviewedBase.isBase5) break;
+        // // };
 
 
 
@@ -64,12 +57,14 @@ function genExercise (size){
         //     break
         // }
 
+        
+        // I guess it's not working
         table.innerHTML += `<tr>${exercise[i]}</tr>`;
         
-        // Updating the result
+        // Calculating the result
         exercise[0] += exercise[i];
         
-        // Calculating the result
+        // Printing the result [needs fix]
         if (i == size){
             console.log("Result: " + exercise[0]);
             table.innerHTML += `<tr>Result: ${exercise[0]}</tr>`;
@@ -82,9 +77,10 @@ function operationGen () {
             name: null,
             multiplier: null,
             symbol: null,
-            isBase5: false,
-            isBase10: false
+            isBase5: null,
+            isBase10: null
         };
+
         let random = Math.random();
         
         if (random<=0.5){
@@ -96,14 +92,18 @@ function operationGen () {
             operation.symbol = "-";
             operation.name = "sub"
         }
+        
         return operation;
 };
 
+// SOMETHING IS HAPPENING HERE WHEN IT RECEIVES AN OBJECT AS A PARAMETER. COMPARE WITH TESTIDENTIFICATION()
 function identifyBase (number1, op, number2){
     let number1OnAbacus = convertToAbacus(number1);
     let number2OnAbacus = convertToAbacus(number2);
 
-    if (op.name == "sub" && number2 <= number1 && number2 <= 4 && number2>number1OnAbacus.bead1){
+    
+
+    if (op.name == "sub" && (number2 <= number1) && (number2 <= 4) && (number2 > number1OnAbacus.bead1)){
         op.isBase5 = true;
     } else if (op.name == "sub" && number2 > number1){
         op.isBase10 = true
@@ -115,21 +115,36 @@ function identifyBase (number1, op, number2){
         op.isBase10 = true;
     }
 
+    console.log("IDENTIFY BASE OBJECT: ");
+    console.log(op);
+
     return op;
+};
+
+function testIdentification(){
+    let op = {
+        name: "add",
+        isBase5: false,
+        isBase10: false
+    }
+
+    op = identifyBase(1,op,9);
+    
+    console.log(`Base 5: ${op.isBase5} | Base 10: ${op.isBase10}`);
 };
 
 function convertToAbacus (number){
     let rod = {
         bead1: 0,
         bead5: 0,
-    }
+    };
 
     if (number >= 5){
         rod.bead5 = 1;
         rod.bead1 = number - 5;
     } else {
         rod.bead1 = number;
-    }
+    };
 
     return rod;
 };
